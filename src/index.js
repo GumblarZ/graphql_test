11c587d1a9aee0e39f8245870e8b5eaa80a0d520
GumblarZ <jc0d3001@outlook.com>
2022-01-30 16:35:24 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        active: Boolean!
+    }
+
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        author: User! 
+    }
     type Query {
         hello: String
+        users: [User]!
+        getUserByEmail(email: String!): User!
     }
 `;
+
+const users =[
+    { _id: String(Math.random()), name: 'Pedro0', email: 'daleste@yahoo.com', active: true},
+    { _id: String(Math.random()), name: 'Pedro1', email: 'cabeca_pika@gmail.com', active: true},
+    { _id: String(Math.random()), name: 'Pedro2', email: 'daleste@gmail.com', active: true},
+    { _id: String(Math.random()), name: 'Pedro', email: 'daleste@gmail.com', active: false}
+];
+
 const resolvers = {
     Query: {
-        hello:()=> "Ola peixe"
+        hello:()=> 'Joao',
+        users: ()=> users,
+        getUserByEmail: (_, args) =>{
+            return users.find((user) => user.email === args.email);
+        }
     }
 };
 
