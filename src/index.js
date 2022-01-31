7ed8cd7ad8ba3150903c19abcc335eafa05d3863
GumblarZ <jc0d3001@outlook.com>
2022-01-31 10:05:04 -0300

    corrigido o erro de modulo
    introduzido type module no package

--- src/index.js
+++ src/index.js
@@ -1,52 +1,5 @@
-const { ApolloServer, gql } = require('apollo-server');
-import users from './data.js';
-const typeDefs = gql`
-    type User {
-        _id: ID!
-        name: String!
-        email: String!
-        active: Boolean!
-    }
+import startServer from "./startServe.js";
+import typeDefs from "./graphql/typeDefs.js";
+import resolvers from "./graphql/resolvers.js";
 
-    type Post {
-        _id: ID!
-        title: String!
-        content: String!
-        author: User! 
-    }
-    type Mutation {
-        createUser(name: String!, email: String!): User!
-    }
-
-    type Query {
-        hello: String
-        users: [User]!
-        getUserByEmail(email: String!): User!
-    }
-`;
-
-
-const resolvers = {
-    Query: {
-        hello:()=> 'Joao',
-        users: ()=> users,
-        getUserByEmail: (_, args) =>{
-            return users.find((user) => user.email === args.email);
-        }
-    },
-    Mutation: {
-        createUser: (_, args) => {
-            const newUser ={
-                _id: String(Math.random()),
-                name: args.name,
-                email: args.email,
-                active: true
-            };
-            users.push(newUser);
-            return newUser;
-        }
-    }
-};
-
-const server = new ApolloServer({ typeDefs, resolvers});
-server.listen().then(({ url }) => console.log(` Esta funcionando cabeca de mizeria ${url}`));
+startServer({ typeDefs, resolvers});
